c8baa7dca0e003dc9f23a4f5d9baeb6f368d6997
swaz786 <shahnawazkits@gmail.com>
2017-03-27 17:46:25 +0530

    add the navigation generate the controller an set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+
   resources :invoices
+  root to:'invoices#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
